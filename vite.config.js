781aaa0a260135a03017a07b9945bebadb7fbed3
Valeriy Siluyanov <siluyanovvaleriy@outlook.com>
2023-03-19 14:42:40 +0300

    New project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
-<<<<<<< HEAD
 export default defineConfig({
   server: {
     https: true,
@@ -9,14 +8,3 @@ export default defineConfig({
   },
   plugins: [mkcert()]
 })
-=======
-
-export default defineConfig({
-  server: {
-    port: 8888,
-    https: true,
-    host: "local.dev",
-  },
-  plugins: [mkcert()],
-});
->>>>>>> 75dc3d5dc7191c84b8b84dba369e5d2e30db03af
